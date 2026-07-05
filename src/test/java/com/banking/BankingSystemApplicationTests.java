78b1b13f1833befcb6868c5039de26fd9a3839bf
Muhaimin Mukammel <muhaiminmukammel818@gmail.com>
2026-07-05 10:59:54 +0600

    Add comment to contextLoads test method
    Added a comment to the contextLoads test method.

--- src/test/java/com/banking/BankingSystemApplicationTests.java
+++ src/test/java/com/banking/BankingSystemApplicationTests.java
@@ -8,6 +8,7 @@ class BankingSystemApplicationTests {
 
     @Test
     void contextLoads() {
+        // This will be untouched, this test is here to check if the application run's or not
     }
 
 }
