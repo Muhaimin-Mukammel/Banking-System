40422ee4e57be1e524eef52a127b3a6e2768604b
Muhaimin Mukammel <muhaiminmukammel818@gmail.com>
2026-07-07 11:31:22 +0600

    quick wins

--- src/main/java/com/banking/BankingSystemApplication.java
+++ src/main/java/com/banking/BankingSystemApplication.java
@@ -1,11 +1,17 @@
 package com.banking;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BankingSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankingSystemApplication.class, args);
+    }
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> System.out.println("Banking System started successfully!");
     }
 }
